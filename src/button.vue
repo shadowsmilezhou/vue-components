f9f67e91cf0c6ddec4cd265ab57e2cbb594c19a3
zhouzhou <630551760@qq.com>
2019-05-28 21:05:00 +0800

    安装parcel，将vue改成单文件应用

---
+++ src/button.vue
@@ -0,0 +1,34 @@
+<template>
+    <button class="g-button">按钮</button>
+</template>
+
+<script>
+    export default {
+        name: "button"
+    }
+</script>
+
+<style lang="scss" >
+
+    body{
+        font-size: var(--font-size);
+    }
+    .g-button {
+        font-size:var(--font-size);
+        height: var(--button-height);
+        padding: 0 1em;
+        border-radius: var(--button-border);
+        background-color: var(--button-bg);
+        &:hover{
+            background-color: var(--border-color-hover);
+        }
+        &:active{
+            background-color: var(--button-active-bg);
+        }
+        &:focus{
+            outline: none;
+        }
+    }
+
+
+</style>
