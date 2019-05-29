da45a527a341771a2728f4c5b3dc5228a3f9a2db
zhouzhou <630551760@qq.com>
2019-05-29 09:35:54 +0800

    1。添加等待状态 2。添加设置样式 3。为按钮添加点击事件

--- src/app.js
+++ src/app.js
@@ -1,9 +1,16 @@
 import  Vue from 'vue'
 import Button from './button'
+import Icon from './icon'
 
 Vue.component('g-button',Button);
+Vue.component('g-icon',Icon);
+
 
 new Vue({
-    el:'#app'
-
+    el:'#app',
+    data:{
+        'loading1':false,
+        'loading2':true,
+        'loading3':false,
+    }
 });
--- src/button.vue
+++ src/button.vue
@@ -1,10 +1,35 @@
+
 <template>
-    <button class="g-button">按钮</button>
+    <button class="g-button" :class="{[`icon-${iconPosition}`]:true}" @click="$emit('click')">
+
+        <g-icon v-if="icon && !loading" :name="icon" class="icon"></g-icon>
+        <g-icon v-if="loading" name="waiting" class="icon waiting"></g-icon>
+        <div class="content">
+            <slot></slot>
+        </div>
+
+    </button>
+
+
 </template>
 
 <script>
     export default {
-        name: "button"
+        name: "g-button",
+        props:{
+            icon:{
+                type:String,
+                default:''
+            },
+            iconPosition:{
+                type: String,
+                default: "left"
+            },
+            loading:{
+                type:Boolean,
+                default:false
+            }
+        }
     }
 </script>
 
@@ -13,12 +38,22 @@
     body{
         font-size: var(--font-size);
     }
+    @keyframes waiting {
+        0%{
+            transform: rotate(0deg);
+        }
+        100%{
+            transform: rotate(360deg);
+        }
+    }
     .g-button {
         font-size:var(--font-size);
         height: var(--button-height);
         padding: 0 1em;
         border-radius: var(--button-border);
         background-color: var(--button-bg);
+        display: inline-flex;justify-content: center;align-items: center;
+        vertical-align: middle;
         &:hover{
             background-color: var(--border-color-hover);
         }
@@ -28,6 +63,28 @@
         &:focus{
             outline: none;
         }
+        > .icon{
+            margin-right:.5em ;
+            order:1
+        }
+        > .content {
+            order:2
+        }
+
+        &.icon-right {
+            > .icon{
+                margin-left: .5em;
+                order: 2;
+            }
+            > .content {
+                order:1;
+            }
+        }
+        > .waiting {
+            animation: waiting 1s infinite linear;
+        }
+
+
     }
 
 
