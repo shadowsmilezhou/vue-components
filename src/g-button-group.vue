523777c0cfce4f2f0a7c0ebde6c5c6d6f865e37c
zhouzhou <630551760@qq.com>
2019-05-29 09:54:42 +0800

    1.第一个元素不需要margin-left 2。g-button-group第一个元素不需要左移

---
+++ src/g-button-group.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="g-button-group">
+        <slot></slot>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: "g-button-group",
+        mounted() {
+            for (let node of this.$el.children){
+                if (node.nodeName.toLocaleLowerCase() !== 'button'){
+                    console.warn(`子元素应该全是button，而你却设置了${node.nodeName}`)
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+    .g-button-group{
+        display: inline-flex;
+        vertical-align: middle;
+        > .g-button {
+            border-radius: 0;
+            &:not(:first-child){
+                margin-left: -1px;
+            }
+
+            &:first-child {
+                border-top-left-radius: var(--border-radius);
+                border-bottom-left-radius: var(--border-radius);
+            }
+            &:last-child {
+                border-top-right-radius: var(--border-radius);
+                border-bottom-right-radius: var(--border-radius);
+            }
+            &:hover {
+                position: relative;
+                z-index: 1;
+            }
+        }
+
+    }
+
+
+</style>
