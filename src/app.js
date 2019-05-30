6dd54905c991bd31246500f148185736abee8acd
zhouzhou <630551760@qq.com>
2019-05-30 17:52:52 +0800

    利用flex实现一般的布局

--- src/app.js
+++ src/app.js
@@ -5,6 +5,11 @@ import GButtonGroup from './g-button-group'
 import Ginput from './g-input'
 import Grow from './g-row'
 import Gcol from './g-col'
+import Glayout from './g-layout'
+import Gcontent from './g-content'
+import Gheader from './g-header'
+import Gfooter from './g-footer'
+import Gslider from './g-slider'
 
 Vue.component('g-button',GButton);
 Vue.component('g-icon',GIcon);
@@ -12,6 +17,11 @@ Vue.component('g-button-group',GButtonGroup);
 Vue.component('g-input',Ginput);
 Vue.component('g-row',Grow);
 Vue.component('g-col',Gcol);
+Vue.component('g-layout',Glayout);
+Vue.component('g-content',Gcontent);
+Vue.component('g-slider',Gslider);
+Vue.component('g-header',Gheader);
+Vue.component('g-footer',Gfooter);
 
 
 new Vue({
---
+++ src/g-slider.vue
@@ -0,0 +1,40 @@
+<template>
+    <transition name="slide">
+        <div :class="[`sider`,{slideChange}]" >
+            <slot></slot>
+            <button @click="slideChange=!slideChange">close</button>
+        </div>
+    </transition>
+</template>
+<script>
+    export default {
+        name: 'g-slider',
+        data () {
+            return {
+                slideChange: false
+            }
+        },
+        methods: {
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .sider {
+        position: relative;
+        > button {
+            position: absolute;
+            top: 0;
+            right: 0;
+        }
+    }
+    .slideChange {
+        width: 3%;
+    }
+    .slide-enter-active, .slide-leave-active {
+        transition: all .3s;
+    }
+    .slide-enter, .slide-leave-to {
+        margin-left: -50px;
+    }
+</style>
